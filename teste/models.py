d1ca0bb2945e778f98a3b65f3ba5f01369471204
rafael <rafael56@hotmail.com>
2015-11-25 13:37:12 -0200

    alteracao de timezone para datatime

--- teste/models.py
+++ teste/models.py
@@ -1,17 +1,18 @@
 from django.db import models
-from django.utils import timezone
+# from django.utils import timezone
+import datetime
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=datetime.datetime.now())
     published_date = models.DateTimeField(
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_date = datetime.datetime.now()()
         self.save()
 
     def __str__(self):
